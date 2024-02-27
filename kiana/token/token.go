11d7af3bcd00edec341cff9020592d6f1ae9dfb2
myk <michaelmugendi1707@gmail.com>
2024-02-27 09:13:04 +0000

    Update: added constants for the language

--- kiana/token/token.go
+++ kiana/token/token.go
@@ -6,3 +6,30 @@ type Token struct {
 	Type TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" //add, foobar, x, y ...
+	INT = "INT" // 1,2,3,4
+
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimeters 
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords 
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
