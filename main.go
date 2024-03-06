3e5b5becb5561fbe88357c1b91be8ffbc8a3ca89
myk <michaelmugendi1707@gmail.com>
2024-03-06 09:13:58 +0000

    Update: added log if port not found

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
 }
