2de9691d5d21552bc099226c56460ed2ca35d327
myk <michaelmugendi1707@gmail.com>
2024-03-06 10:19:41 +0000

    Update: added chi package for router

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module github.com/myk-francis/go-rssagg
+module github.com/myk-francis/learning-go
 
 go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.0.12
--- main.go
+++ main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port:", portString)
 }
