284a886a4223e1eca9ea41c4a122c0917a6416d2
myk <michaelmugendi1707@gmail.com>
2024-03-09 14:36:53 +0000

    Update: added modals

---
+++ models.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/myk-francis/learning-go/internal/database"
+)
+
+type User struct {
+	ID		uuid.UUID `json:"id"`
+	CreatedAt		time.Time `json:"created_at"`
+	UpdatedAtAt		time.Time `json:"updated_at"`
+	Name		string `json:"name"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID: dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAtAt: dbUser.UpdatedAt,
+		Name: dbUser.Name,
+	}
+}
