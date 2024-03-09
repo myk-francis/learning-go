1ef825dd1eacfe0ed2cd6eecfbafc4e690b697e7
myk <michaelmugendi1707@gmail.com>
2024-03-09 15:11:51 +0000

    Update: handlerGetUser

--- handler_user.go
+++ handler_user.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/myk-francis/learning-go/internal/database"
+	"github.com/myk-francis/learning-go/internal/database/auth"
 )
 
 func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,24 @@ func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request
 
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+
+func (apiCfg *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
 	}
 
 	respondWithJSON(w, 200, databaseUserToUser(user))
--- internal/database/auth/auth.go
+++ internal/database/auth/auth.go
@@ -13,16 +13,16 @@ import (
 func GetAPIKey(headers http.Header) (string, error) {
 	val := headers.Get("Authorization")
 	if val == "" {
-		return "", errors.New("No authentication info found")
+		return "", errors.New("no authentication info found")
 	}
 
 	vals := strings.Split(val, " ")
 	if len(vals) != 2 {
-		return "", errors.New("Malformed auth headers")
+		return "", errors.New("malformed auth headers")
 	}
 
 	if vals[0] != "ApiKey" {
-		return "", errors.New("Malformed first part of auth headers")
+		return "", errors.New("malformed first part of auth headers")
 	}
 
 	return vals[1], nil
