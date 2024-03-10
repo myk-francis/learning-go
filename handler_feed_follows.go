ac4e2eac13ab21774dc2fc7c32867b2c65e93b2f
myk <michaelmugendi1707@gmail.com>
2024-03-10 09:05:55 +0000

    Update: implementated handlerDeleteFeedFollows

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/myk-francis/learning-go/internal/database"
 )
@@ -50,6 +51,28 @@ func (apiCfg *apiConfig)handlerGetFeedFollows(w http.ResponseWriter, r *http.Req
 
 	respondWithJSON(w, 201, databaseFeedsFollowsToFeedsFollows(feedFollows))
 }
+
+func (apiCfg *apiConfig)handlerDeleteFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't parse feed follow ID: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollows(r.Context(), database.DeleteFeedFollowsParams{
+		ID: feedFollowID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't delete feed follow: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, struct{}{})
+	
+}
+
 
 
 
