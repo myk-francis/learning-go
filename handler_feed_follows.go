8ee3a3923cb7d544626cacf0f4368ca7a7b96b62
myk <michaelmugendi1707@gmail.com>
2024-03-10 08:48:51 +0000

    Update: implemented handlerGetFeedFollows

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -40,32 +40,15 @@ func (apiCfg *apiConfig)handlerCreateFeedFollow(w http.ResponseWriter, r *http.R
 }
 
 func (apiCfg *apiConfig)handlerGetFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
-	type parameters struct {
-		FeedID uuid.UUID `json:"feed_id"`
-	}
-	decoder := json.NewDecoder(r.Body)
 
-	params := parameters{}
-	err := decoder.Decode(&params)
+	feedFollows, err := apiCfg.DB.GetFeedFollows(r.Context(), user.ID)
+
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get feed follow: %v", err))
 		return
 	}
 
-	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
-		ID: uuid.New(),
-		CreatedAt: time.Now().UTC(),
-		UpdatedAt: time.Now().UTC(),
-		UserID: user.ID,
-		FeedID: params.FeedID,
-	})
-
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed follow: %v", err))
-		return
-	}
-
-	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
+	respondWithJSON(w, 201, databaseFeedsFollowsToFeedsFollows(feedFollows))
 }
 
 
