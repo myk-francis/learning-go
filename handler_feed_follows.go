99712c1b1953f28f804cb38fc8665bef40dba415
myk <michaelmugendi1707@gmail.com>
2024-03-10 08:28:53 +0000

    Update: implemented CreateFeedFollow

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -23,17 +23,16 @@ func (apiCfg *apiConfig)handlerCreateFeedFollow(w http.ResponseWriter, r *http.R
 		return
 	}
 
-	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+	feed, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
 		ID: uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
-		Name: params.Name,
-		Url: params.URL,
 		UserID: user.ID,
+		FeedID: params.FeedID,
 	})
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed follow: %v", err))
 		return
 	}
 
