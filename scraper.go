1058417b541e9c8236654f15a543bf6826376705
myk <michaelmugendi1707@gmail.com>
2024-03-11 08:07:33 +0000

    Update: scrapeFeed

--- scraper.go
+++ scraper.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/myk-francis/learning-go/internal/database"
 )
 
-func startScrapint(db *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
+func startScraping(db *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
 	log.Printf("Scraping on %v goroutines every %s duration", concurrency, timeBetweenRequest)
 
 	ticker := time.NewTicker(timeBetweenRequest)
 	for ; ; <-ticker.C {
-		
+		feeds, err := db.GetNextFeedsToFetch(context.Background(), int32(concurrency))
+		if err != nil {
+			log.Println("Error fetching feeds:", err)
+			continue
+		}
+
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+
+			go scrapeFeed(wg)
+		}
+		wg.Wait()
+
 	}
+
+	
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed){
+	defer wg.Done()
 }
