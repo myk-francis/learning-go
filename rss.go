3f478d01370a67f4ce257a9301ec3bb7ef478142
myk <michaelmugendi1707@gmail.com>
2024-03-11 07:33:22 +0000

    Update: created urlToFeed

--- rss.go
+++ rss.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+)
+
 type RSSFeed struct {
 	Channel struct {
 		Title string `xml:"title"`
@@ -15,4 +22,31 @@ type RSSItem struct {
 	Link string `xml:"link"`
 	Description string `xml:"description"`
 	PubDate string `xml:"pubDate"`
+}
+
+func urlToFeed(url string) (*RSSFeed, error){
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	rssFeed := RSSFeed{}
+
+	xml.Unmarshal(dat, &rssFeed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rssFeed, err
 }
