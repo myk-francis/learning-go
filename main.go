f6f7e6480531307d4d621fac0905d3e07ced12bc
myk <michaelmugendi1707@gmail.com>
2024-03-11 07:38:38 +0000

    Update: changed main file

--- main.go
+++ main.go
@@ -20,6 +20,13 @@ type apiConfig struct{
 }
 
 func main() {
+	feed,err := urlToFeed("https://wagslane.dev/index.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(feed)
+
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
