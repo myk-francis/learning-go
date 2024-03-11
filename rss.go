123190c69e537459a6a4849e39c9fae9c07bb709
myk <michaelmugendi1707@gmail.com>
2024-03-11 07:34:29 +0000

    Update: changed urlToFeed

--- rss.go
+++ rss.go
@@ -24,29 +24,29 @@ type RSSItem struct {
 	PubDate string `xml:"pubDate"`
 }
 
-func urlToFeed(url string) (*RSSFeed, error){
+func urlToFeed(url string) (RSSFeed, error){
 	httpClient := http.Client{
 		Timeout: 10 * time.Second,
 	}
 
 	resp, err := httpClient.Get(url)
 	if err != nil {
-		return nil, err
+		return RSSFeed{}, err
 	}
 
 	defer resp.Body.Close()
 
 	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return RSSFeed{}, err
 	}
 
 	rssFeed := RSSFeed{}
 
 	xml.Unmarshal(dat, &rssFeed)
 	if err != nil {
-		return nil, err
+		return RSSFeed{}, err
 	}
 
-	return &rssFeed, err
+	return rssFeed, err
 }
